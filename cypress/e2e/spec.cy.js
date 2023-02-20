9f6ad49c5d400f4fe093a3dad29d20c7efd5bb71
nhanisahy <124703381+nhanisahy@users.noreply.github.com>
2023-02-21 02:43:11 +0800

    Updated spec.cy.js and todomvcreact.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('My first test following Cypress.io documentation', () => {
+  
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
   })
+
+  it('Visits the Kitchen Sink', () =>{
+    cy.visit('https://example.cypress.io')
+    //gets the content "type"
+    cy.contains('type')
+    //click the "type" element
+    cy.contains('type').click()
+    //check that the url contains '/commands/action'
+    cy.url().should('include', '/commands/action')
+    //get an input
+    cy.get('.action-email')
+      //type into it
+      .type('fake-email@email.com')
+      //verify that the value has been updated as per previous input
+      .and('have.value', 'fake-email@email.com')
+  })
+
 })
--- cypress/e2e/todomvcreact.cy.js
+++ cypress/e2e/todomvcreact.cy.js
@@ -2,10 +2,51 @@
 
   it('loads ToDoMVC React page and adds one task', () => {
     cy.visit('https://todomvc.com/examples/react/#/')
-    cy.get('.new-todo')
-        .type('Drink a glass of water')
-        .type('{enter}')
+    cy.get('.new-todo').type('Drink a glass of water{enter}')
     cy.get('.view > label')
         .should('be.visible')
         .and('contain.text', 'Drink a glass of water')
   })
+
+  it('adds multiple unique todos', () => {
+    cy.visit('https://todomvc.com/examples/react/#/')
+    //practising Cypress' way of command-action command-action instead of command-action-action
+    cy.get('.new-todo').type('Open the bedroom windows{enter}')
+    cy.get('.new-todo').type('Do your morning stretch exercise!{enter}')
+    //confirm that the list now contains 2 items
+    cy.get('.view').should('have.length', 2)
+  })
+
+  it('creates 3 todos, marks 1 task Completed, and 2 as Active', () => {
+    cy.visit('https://todomvc.com/examples/react/#/')
+    //create 3 todos
+    cy.get('.new-todo').type('stretch{enter}')
+    cy.get('.new-todo').type('iron shirt{enter}')
+    cy.get('.new-todo').type('make bed{enter}')
+    //verify the length and texts of the todos
+    cy.get('.view').should('have.length', 3)
+    cy.get('.todo-list li').first().should('have.text', 'stretch')
+    cy.get('.todo-list li').last().should('have.text', 'make bed')
+    cy.get('.todo-list li').eq(1).should('have.text', 'iron shirt')
+    //select the first todo (this marks it completed)
+    cy.get('.toggle').first().click()
+    cy.get('.toggle').first().should("be.checked")
+    //verify that the selected first to do is in the status Completed
+    cy.contains('a', 'Completed').click()
+    cy.contains('stretch').should('be.visible')
+    //view display for Active tasks
+    cy.contains('a', 'Active').click()
+    cy.contains('make bed').should('be.visible')
+    cy.contains('iron shirt').should('be.visible')
+    //view all todos
+    cy.contains('a', 'All').click()
+    cy.contains('stretch').should('be.visible')
+    cy.get('.toggle').first().should("be.checked")
+    cy.contains('make bed').should('be.visible')
+    cy.contains('iron shirt').should('be.visible')
+    //trigger Clear Completed function
+    cy.get('.clear-completed').click()
+    //click on Completed section to verify
+    cy.contains('a', 'Completed').click()
+    cy.get('.selected').contains('Completed')
+    })
